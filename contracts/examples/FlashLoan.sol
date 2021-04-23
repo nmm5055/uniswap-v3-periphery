e4fc99e13ebca57904d3c27dfffc79f418167cd7
Sara Reynolds <snreynolds2506@gmail.com>
2021-04-23 12:56:25 -0400

    pseudo code

--- contracts/examples/FlashLoan.sol
+++ contracts/examples/FlashLoan.sol
@@ -10,8 +10,6 @@ import '../base/PeripheryImmutableState.sol';
 
 import '../libraries/PoolAddress.sol';
 import '../libraries/CallbackValidation.sol';
-import '../SwapRouter.sol';
-import '../interfaces/ISwapRouter.sol';
 
 
 abstract contract FlashLoan is IUniswapV3FlashCallback, PeripheryPayments, PeripheryImmutableState {
@@ -25,19 +23,11 @@ abstract contract FlashLoan is IUniswapV3FlashCallback, PeripheryPayments, Perip
         FlashCallbackData memory data = abi.decode(data, (FlashCallbackData));
         CallbackValidation.verifyCallback(factory, data.poolKey);
 
-        //(address token0, address token1, uint24 fee1) = data.path.decodeFirstPool();
+        (address token0, address token1, uint24 fee1) = data.path.decodeFirstPool();
         
-
-        // call exactInput with the path for both swaps
-        ExactInputParams inputParamsSwapOne = ExactInputParams({path: data.path1, recipient: data.payer, deadline: 0, amountIn: data.amount0, amountOutMinimum: data.amount0});
-
-        ExactInputParams inputParamsSwapTwo = ExactInputParams({path: data.path2, recipient: data.payer, deadline: 0, amountIn: data.amount1, amountOutMinimum: data.amount1});
+        // call exactInputSingle for swapping token1 for token0 in pool w/fee1
         
-        // call exactInput for swapping token1 for token0 in pool w/fee1
-        uint256 amountOut0 = exactInput(inputParamsSwapOne);
-
-        // call exactInputfor swapping token0 for token 1 in pool w/fee2
-        uint256 amountOut1 = exactInput(inputParamsSwapTwo);
+        // call exactInputSingle for swapping token0 for token 1 in pool w/fee2
 
         // end up with amountOut0 of token0 from first swap and amountOut1 of token1 from second swap
 
@@ -83,14 +73,13 @@ abstract contract FlashLoan is IUniswapV3FlashCallback, PeripheryPayments, Perip
         // amount of token1 requested to borrow
         // need amount 0 and amount1 in callback to pay back pool
 
-        bytes path1 = abi.encodePacked(params.token0, params.fee1, params.token1);
-        bytes path2 = abi.encodePacked(params.token0, params.fee2, params.token1);
-        
+        // encodes the two pools for the arbitrage opportunity
+        bytes fullPath = abi.encodePacked(params.token0, params.fee1, params.token1, params.token0, params.fee2, params.token1);
         pool.flash(
             params.recipient,
             params.amount0,
             params.amount1,
-            abi.encode(FlashCallbackData({amount0: params.amount0, amount1: params.amount1, payer: msg.sender, poolKey: poolKey, path1: path1, path2: path2}))
+            abi.encode(FlashCallbackData({amount0: params.amount0, amount1: params.amount1, payer: msg.sender, poolKey: poolKey, path: fullPath}))
         );
     }
 
